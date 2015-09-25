d652ad001a5ead3a6ac7906736f561d555b946c9
dbecks <dbecker1226@gmail.com>
2015-09-24 21:18:24 -0700

    Started making a setup function that abstracts the WDC functionality

--- FacebookWDC/wdc-facebook.js
+++ FacebookWDC/wdc-facebook.js
@@ -5,6 +5,122 @@
           to have an auth command and a data fetching/formatting step
    */
 
+  function functionToPromise(fn, ctx) {
+    var promise = $.Deferred();
+    if (fn) {
+      fn.call(ctx, promise.resolve);
+    } else {
+      promise.resolve();
+    }
+  }
+
+  function setup(config) {
+    var _schema, _metaData, _password;
+
+    if(!config.fetchData) {
+      throw new Error('setup requires a fetchData(password, lastRecordToken, data, cb) function.');
+    }
+
+    // Set up the password if one is needed
+    var passwordPromise = functionToPromise(config.getPassword, config);
+
+    // Set up the connector
+    var connector = tableau.makeConnector();
+
+    connector.init = function() {
+
+      var operationsPromiseChain = passwordPromise;
+
+      if (tableau.phase === tableau.phaseEnum.authPhase ||
+          tableau.phase === tableau.phaseEnum.interactivePhase)
+      {
+        // Fetch password information
+        operationsPromiseChain = operationsPromiseChain
+          .then(function() { return passwordPromise; })
+          .then(function(password) {
+            if(password == null) password = _password;
+            tableau.password = password;
+          });
+      }
+
+      if(tableau.phase === tableau.phaseEnum.interactivePhase) {
+        // fetch additional information if more information is needed.
+        operationsPromiseChain
+          .then(function() { return functionToPromise(config.fetchSetupData, config); })
+          .then(function(schema, metaData) {
+            if(schema == null) schema = _schema;
+            if(metaData == null) metaData = _metaData;
+
+            if(!schema) {
+              throw new Error('A schema was never set. Please pass in a fetchSetupData handler to setup() or call context.setSchema() on the returned object');
+            }
+
+            tableau.connectionData = JSON.stringify({ schema: schema, metaData: metaData });
+            tableau.submit();
+          });
+      }
+    };
+
+    connector.getColumnHeaders = function() {
+      var connectionData = JSON.parse(tableau.connectionData);
+      var headers = WDCSchema.convertToTableHeaders(connectionData.schema);
+      var fieldNames = _.keys(headers);
+      var fieldTypes = _.values(headers);
+
+      tableau.headersCallback(fieldNames, fieldTypes);
+    };
+
+    connector.getTableData = function(lastRecordToken) {
+      var connectionData = JSON.parse(tableau.connectionData);
+
+      // TODO: Create a way to pass in errors or pass in progress if reading in a lot of data, maybe give a deffered? or is that too complicated?
+      config.fetchData(tableau.password, lastRecordToken, connectionData.data, function(resultData, lastRecordToken) {
+        tableau.dataCallback(resultData, lastRecordToken, false);
+      });
+    };
+
+    tableau.registerConnector(connector);
+
+
+
+
+
+
+
+    return {
+      setSchema: function(schema) {
+        _schema = schema;
+      },
+      setConnectionName: function(name) {
+        tableau.connectionName = name;
+      },
+      setConnectionData: function(data) {
+        _metaData = data;
+      },
+      getConnectionData: function() {
+        if(typeof _metaData !== 'object') return _metaData;
+
+        // Return a deep clone to keep _data immutable
+        return $.extend(true, Array.isArray(_metaData) ? [] : {}, data);
+      }
+    }
+  }
+
+  var context = setup({
+    fetchPassword: function(cb) {
+
+    },
+
+    fetchSetupData: function(cb) {
+
+    },
+
+    //fetchData: function(password, lastRecordToken, metaData, done, progress, error) { // Do we want this instead?
+    fetchData: function(password, lastRecordToken, metaData, cb) {
+
+    }
+  });
+
   // States
   var documentReady = $.Deferred();
   var fbConnected = $.Deferred();
